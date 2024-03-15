7d89dd926e0ed5ea6dae51c3a7af80cac1574468
Andrea Baccega <vekexasia@gmail.com>
2024-03-16 00:23:28 +0100

    workflow stuff

--- build/reinstalldeps.sh
+++ build/reinstalldeps.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 rm -rf node_modules;
-yarn
+yarn install --immutable;
 cd packages;
 ordered_packages=("bigint-uint8array" "bigint-constrained" "bigint-math" "bigint-buffer-polyfill")
 for package in "${ordered_packages[@]}"; do
   cd $package;
   rm -rf node_modules;
-  yarn
+  yarn install --immutable
   cd ..;
 done
+cd ..
