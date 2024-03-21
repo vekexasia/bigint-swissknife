a6f4db335fe7fd3cd6392d77a6bb62ef6b9a45f2
Andrea Baccega <vekexasia@gmail.com>
2024-03-21 20:40:30 +0100

    fix #1 by removing dependency on bigint-buffer and making it peer

--- packages/bigint-uint8array/vitest.workspace.ts
+++ packages/bigint-uint8array/vitest.workspace.ts
@@ -1,23 +1,35 @@
-import { defineWorkspace } from 'vitest/config'
+import {defineWorkspace} from 'vitest/config'
 import path from 'path'
+
 export default defineWorkspace([
   {
     extends: path.join(__dirname, 'vitest.config.mts'),
     test: {
       include: ['test/*.test.ts', 'test/node-only/**/*.test.ts'],
       environment: 'node',
-      name: 'node-uint8array',
-      alias: {
-        './browser.js': './native.js'
-      }
+      name: 'node-uint8array-with-bigint-buffer',
     }
   },
+  process.env.MODE !== 'benchmark' ? {
+    extends: path.join(__dirname, 'vitest.config.mts'),
+    test: {
+      include: ['test/*.test.ts', 'test/node-only/**/*.test.ts'],
+      environment: 'node',
+      name: 'node-uint8array-without-bigint-buffer',
+      alias: {
+        'bigint-buffer': 'vekexasia is for hire :)'
+      },
+    }
+  } : {},
   process.env.MODE !== 'benchmark' ? {
     extends: path.join(__dirname, 'vitest.config.mts'),
     test: {
       include: ['test/*.test.ts'],
       environment: 'jsdom',
       name: 'browser-uint8array',
+      alias: {
+        './native.js': './browser.js'
+      },
       browser: {
         name: 'chrome',
         enabled: true,
@@ -25,5 +37,5 @@ export default defineWorkspace([
       }
 
     }
-  }: {},
+  } : {},
 ])
